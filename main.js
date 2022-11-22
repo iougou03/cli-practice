105ec015b271c1170f8391c03c7886fe02733f04
keepgo-studio <keepgo.studio@gmail.com>
2022-11-22 23:00:12 +0900

    Commition from client branch

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
+require('dotenv').config();
+
 const { program } = require('commander');
+const { Octokit } = require('octokit');
+
+const { GITHUB_ACCESS_TOKEN } = process.env;
 
 program.version('0.0.1');
 
+const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
+
 program
-    .command('list')
-    .description('list all arguments')
-    .action(() => {
-        console.log("you\'ve typed", process.argv)
+  .command('me')
+  .description('Check my profile')
+  .action(async () => {
+    const {
+      data: { login },
+    } = await octokit.rest.users.getAuthenticated();
+
+    console.log(login);
+  });
+
+program
+  .command('list')
+  .description('list all arguments')
+  .action(async () => {
+    const result = await octokit.rest.issues.listForRepo({
+      owner: 'iougou03',
+      repo: 'cli-practice',
+      labels: 'bug'
+    });
+
+    // const issuesWithBugLabel = result.data.filter(
+    //   (issue) =>
+    //     issue.labels.find((label) => label.name === 'bug') !== undefined
+    // );
+
+    // issuesWithBugLabel.forEach(issue => {
+    //     console.log(issue.number, issue.title, issue.created_at)
+    // })
+
+    result.data.forEach(issue => {
+        console.log(issue.number, issue.title, issue.created_at)
     })
-program
-    .option('-d, --debug', 'default extra debugging')
 
-program.parse(process.argv)
+  });
 
-const options = program.opts();
+// program
+//     .option('-d, --debug', 'default extra debugging')
 
-if (options.debug) console.log(options)
+// program.parse(process.argv)
+program.parseAsync();
 
+// const options = program.opts();
+
+// if (options.debug) console.log(options);
